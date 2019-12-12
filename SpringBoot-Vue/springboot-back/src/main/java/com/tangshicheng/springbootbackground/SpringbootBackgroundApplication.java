389cb2767adf7efb78603f2153d88930bb247b17
LAPTOP-GN3LAP7B\admin <458730548@qq.com>
2019-12-12 11:00:53 +0800

    提交sprinboot后端

---
+++ SpringBoot-Vue/springboot-back/src/main/java/com/tangshicheng/springbootbackground/SpringbootBackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.tangshicheng.springbootbackground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackgroundApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (SpringbootBackgroundApplication.class, args);
+    }
+
+}
